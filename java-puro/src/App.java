7806fd9c4b13712569bfbc3d71bb9a2d24232a0d
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2023-10-02 13:11:15 -0300

    adicionando replicação dos padrões Strategy e Facade com java puro

--- java-puro/src/App.java
+++ java-puro/src/App.java
@@ -1,6 +1,11 @@
+import padroes.facade.Facade;
 import padroes.singleton.eager.EagerSingleton;
 import padroes.singleton.holder.LazyHolderSingleton;
 import padroes.singleton.lazy.LazySingleton;
+import padroes.strategy.ComportamentoAgressivo;
+import padroes.strategy.ComportamentoDefensivo;
+import padroes.strategy.ComportamentoNormal;
+import padroes.strategy.Robo;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -27,6 +32,32 @@ public class App {
         LazyHolderSingleton instanciaLazyHolderSingleton = LazyHolderSingleton.instancia();
         System.out.println(instanciaLazyHolderSingleton);
         System.out.println(instanciaLazyHolderSingleton);
+        System.out.println("");
         // Retornam o mesmo endereço de memória.
+
+
+        // Testanto o Strategy:
+        System.out.println("Strategy:");
+        ComportamentoAgressivo agressivo = new ComportamentoAgressivo();
+        ComportamentoDefensivo defensivo = new ComportamentoDefensivo();
+        ComportamentoNormal normal = new ComportamentoNormal();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        System.out.println("");
+
+
+        // Testando o Facade:
+        System.out.println("Facade:");
+        Facade facade = new Facade();
+		facade.migrarCliente("Fulano", "01001000");
     }
 }
